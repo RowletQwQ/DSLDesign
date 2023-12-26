7b45a5c1d292e5e581a489ea80adcf828563f683
YT N <2474095154@qq.com>
2023-12-27 00:11:22 +0800

    完成文档编写

--- README.md
+++ README.md
@@ -7,55 +7,72 @@
 ![1703516340495](image/README/1703516340495.png)
 
 ## 用法
+
 安装依赖
+
 ```shell
 yarn install && cd client && yarn install
 ```
 
 启动服务端
+
 ```shell
 yarn start
 ```
 
 启动客户端
+
 ```shell
 cd client && yarn start
 ```
 
 执行自动化测试
+
 ```shell
 yarn test
 ```
 
 手动输入，测试表达式
+
 ```shell
 yarn test:calc
 ```
 
 手动输入语法，展示语法树
+
 ```shell
 yarn test:ast
 ```
 
 运行本地命令行交互版本(采用docs/example.sc作为脚本文件执行)
+
 ```shell
 yarn cli
 ```
 
 在doc/api目录下生成程序内api文档
+
 ```shell
 yarn doc
 ```
 
 按照src/parser目录下的antlr4语法文件生成对应的可调用ts模板
+
 ```shell
 yarn antlr4
 ```
 
 ## 语法说明
+
 见[语法说明](docs/语法说明.md)
 
-## 项目结构说明(详细实现原理请移步[完整说明](docs/详细说明.md))
+## API文档
+见[API文档](docs/api/index.html)
+
+## 项目结构说明
+
+(详细实现原理请移步[完整说明](docs/详细说明.md))
+
 ```
 .
 ├── README.md
@@ -84,3 +101,4 @@ yarn antlr4
 │   ├── index.tsx # 服务端入口
 │   ├── exec.ts # 本地执行CLI入口
 ├── test # 测试用例
+```
--- src/calc_test.ts
+++ src/calc_test.ts
@@ -1,56 +1,58 @@
 // calc_test.ts
-/**
- * @module calc_test
- * A simple calculator that can calculate expressions.
- * Use for testing the parser and visitor.
- */
+
 import { CharStreams, CommonTokenStream } from "antlr4ng";
 import { cslLexer } from "./parser/cslLexer.js";
 import { cslParser } from "./parser/cslParser.js";
 import { ExpressionVisitor } from "./visitor/expression_visitor.js";
 import { createInterface } from "readline";
 
-
-
 const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
+
+/**
+ * @module calc_test
+ * A simple calculator that can calculate expressions.
+ * Use for testing the parser and visitor.
+ */
 export class TestCalcExpression {
     /**
      * Handles the execution of an expression script.
      * @param expr_script - The expression script to be executed.
      * @returns void
      */
-  handle_expr(expr_script: string): void {
-    if (expr_script.toLowerCase() === 'q') {
-        process.exit(0);
-    }
-    
-    let lexer = new cslLexer(CharStreams.fromString(expr_script));
-    let token = new CommonTokenStream(lexer);
-    let parser = new cslParser(token);
-    let visitor = new ExpressionVisitor();
-    let expr = visitor.visit(parser.expression());
-    if (expr == null) {
-        throw new Error("expr is null");
-    }
-    console.log(expr.try_get_value());
-    console.log("Input the expression you want to calculate, or exit with 'q':");
-    setImmediate(() => {
-        rl.question(">>>", (answer) => {
-            this.handle_expr(answer);
+    handle_expr(expr_script: string): void {
+        if (expr_script.toLowerCase() === "q") {
+            process.exit(0);
+        }
+
+        let lexer = new cslLexer(CharStreams.fromString(expr_script));
+        let token = new CommonTokenStream(lexer);
+        let parser = new cslParser(token);
+        let visitor = new ExpressionVisitor();
+        let expr = visitor.visit(parser.expression());
+        if (expr == null) {
+            throw new Error("expr is null");
+        }
+        console.log(expr.try_get_value());
+        console.log(
+            "Input the expression you want to calculate, or exit with 'q':"
+        );
+        setImmediate(() => {
+            rl.question(">>>", (answer) => {
+                this.handle_expr(answer);
+            });
         });
-    });
-}
+    }
 }
 
 try {
-    let test = new TestCalcExpression();
-    console.log("Input the expression you want to calculate, or exit with 'q':");
-    rl.question(">>>", (answer) => {
-        test.handle_expr(answer);
-    });
+  let test = new TestCalcExpression();
+  console.log("Input the expression you want to calculate, or exit with 'q':");
+  rl.question(">>>", (answer) => {
+    test.handle_expr(answer);
+  });
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
